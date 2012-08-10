800515cc60acb6522c7a966fa08322caa1dc2e90
sgtkwol <sgtkwol@gmail.com>
2012-08-10 19:29:39 -0400

    Initial epicmtd commit, copy and paste of i9100

---
+++ products/pa_epicmtd.mk
@@ -0,0 +1,17 @@
+# Check for target product
+ifeq (pa_i9100,$(TARGET_PRODUCT))
+
+# include ParanoidAndroid common configuration
+include vendor/pa/config/pa_common.mk
+
+# OVERLAY_TARGET adds overlay asset source
+OVERLAY_TARGET := $(TARGET_PRODUCT)
+
+# Inherit CM9 device configuration
+$(call inherit-product, device/samsung/i9100/cm.mk)
+
+PRODUCT_NAME := pa_i9100
+
+GET_VENDOR_PROPS := $(shell vendor/pa/tools/getvendorprops.py $(PRODUCT_NAME))
+
+endif
