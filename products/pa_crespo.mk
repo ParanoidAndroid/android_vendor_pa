6e2cadbdea477dfa8170209855663bf0d743cc27
Daniel <daxx4182@gmail.com>
2012-08-31 07:44:51 +0800

    Nexus S missing props
    CM didn't get this up for Nexus S. Builds might not work properly without these.
    
    Change-Id: Ie0946723756a0807404f44c284faba4f7efddbfb

--- products/pa_crespo.mk
+++ products/pa_crespo.mk
@@ -10,6 +10,14 @@ OVERLAY_TARGET := pa_hdpi
 # include ParanoidAndroid common configuration
 include vendor/pa/config/pa_common.mk
 
+# include missing proprietaries
+PRODUCT_COPY_FILES += \
+  vendor/pa/proprietary/crespo/bcm4329.hcd:system/vendor/firmware/bcm4329.hcd \
+  vendor/pa/proprietary/crespo/libpn544_fw.so:system/vendor/firmware/libpn544_fw.so \
+  vendor/pa/proprietary/crespo/gps.s5pc110.so:system/vendor/lib/hw/gps.s5pc110.so \
+  vendor/pa/proprietary/crespo/libakm.so:system/vendor/lib/libakm.so \
+  vendor/pa/proprietary/crespo/gpsd:system/vendor/bin/gpsd
+
 # Inherit CM device configuration
 $(call inherit-product, device/samsung/crespo/cm.mk)
 
