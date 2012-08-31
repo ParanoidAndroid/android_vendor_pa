a57463301a8d79b79b0de8d36d383cfca80d259f
Androguide.fr <admin@androguide.fr>
2012-09-01 00:07:07 +0200

    Add Galaxy Tab 7.7 wifi and 3g (p6810 & p6800)
    Change-Id: I29db4cf0c1fd2becbd7cf18c7dd8e55ddf29a588

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -17,3 +17,5 @@ add_lunch_combo pa_urushi-userdebug
 add_lunch_combo pa_crespo-userdebug
 add_lunch_combo pa_crespo4g-userdebug
 add_lunch_combo pa_quincyatt-userdebug
+add_lunch_combo pa_p6810-userdebug
+add_lunch_combo pa_p6800-userdebug
