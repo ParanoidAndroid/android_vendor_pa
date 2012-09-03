4f1a42df0ee09e39dfe435e9ef14fb966ed7b599
scotthartbti <scott.hart.bti@gmail.com>
2012-09-02 21:39:42 -0700

    Add Infuse4g to the PA building source files
    Change-Id: Icdbb3a12a531aead9a1a5a7a2659a2ce1767e4ea

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,3 +20,4 @@ add_lunch_combo pa_quincyatt-userdebug
 add_lunch_combo pa_p6810-userdebug
 add_lunch_combo pa_p6800-userdebug
 add_lunch_combo pa_captivatemtd-userdebug
+add_lunch_combo pa_infuse4g-userdebug
