df6339e259e22fecc21c66459485b0daf2a9c260
gokussjx <gokussjx@gmail.com>
2012-10-17 10:41:04 +0530

    Add entry for fetching ParanoidPreferences.apk
    Change-Id: I1a632b7a83d5c762eabd49a99fa87a12dde7a27c

--- config/pa_common.mk
+++ config/pa_common.mk
@@ -9,6 +9,7 @@ PRODUCT_COPY_FILES += \
     vendor/pa/prebuilt/common/bin/backuptool.sh:system/bin/backuptool.sh \
     vendor/pa/prebuilt/common/apk/GooManager.apk:system/app/GooManager.apk \
     vendor/pa/prebuilt/common/apk/SuperSU.apk:system/app/SuperSU.apk \
+    vendor/pa/prebuilt/common/apk/ParanoidPreferences.apk:system/app/ParanoidPreferences.apk \
     vendor/pa/prebuilt/common/xbin/su:system/xbin/su
 
 ifneq ($(PARANOID_BOOTANIMATION_NAME),)
