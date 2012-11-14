8fce1b275cd25aea762cf0f0c19d4b624e742864
Jesús David <jedga_95@outlook.com>
2012-11-13 19:47:30 -0500

    Add basic environment for maguro building
    Required changes for aosp building (Android 4.2)
    
    Change-Id: I35b85940244ccbf85205f6181b6c8ab6d6164cd7

--- products/pa_maguro.mk
+++ products/pa_maguro.mk
@@ -1,3 +1,17 @@
+# Copyright (C) 2012 ParanoidAndroid Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Check for target product
 ifeq (pa_maguro,$(TARGET_PRODUCT))
 
@@ -12,6 +26,13 @@ PREFS_FROM_SOURCE := true
 
 # include ParanoidAndroid common configuration
 include vendor/pa/config/pa_common.mk
+
+## Override AOSP build properties
+PRODUCT_NAME := pa_maguro
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Galaxy Nexus
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_FINGERPRINT="google/yakju/maguro:4.0.4/IMM76I/330937:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.0.4 IMM76I 330937 release-keys"
 
 # include missing proprietaries
 PRODUCT_COPY_FILES += \
