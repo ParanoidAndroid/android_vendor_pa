799d6b938bd88dab677795f45ae22b87662bcfa0
wjb <jaybob413@gmail.com>
2012-11-21 17:06:28 -0500

    Update grouper product make file for 4.2
    Change-Id: I925964f23873c86e1977d174b5e96bd74e19b13d

--- products/pa_grouper.mk
+++ products/pa_grouper.mk
@@ -1,3 +1,17 @@
+# Copyright (C) 2012 ParanoidAndroid Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Check for target product
 ifeq (pa_grouper,$(TARGET_PRODUCT))
 
@@ -7,14 +21,25 @@ PARANOID_BOOTANIMATION_NAME := XHDPI
 # OVERLAY_TARGET adds overlay asset source
 OVERLAY_TARGET := pa_tvdpi
 
-# include ParanoidAndroid common configuration
+# Build paprefs from sources
+PREFS_FROM_SOURCE := true
+
+# Include ParanoidAndroid common configuration
 include vendor/pa/config/pa_common.mk
 
 # include missing proprietaries
 PRODUCT_COPY_FILES += \
   vendor/pa/proprietary/grouper/bcm4330.hcd:system/etc/firmware/bcm4330.hcd
 
+# Inherit AOSP device configuration
+$(call inherit-product, device/asus/grouper/full_grouper.mk)
+
+# Override AOSP build properties
 PRODUCT_NAME := pa_grouper
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Nexus 7
+PRODUCT_MANUFACTURER := Asus
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_FINGERPRINT="google/yakju/maguro:4.0.4/IMM76I/330937:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.0.4 IMM76I 330937 release-keys"
 
 GET_VENDOR_PROPS := $(shell vendor/pa/tools/getvendorprops.py $(PRODUCT_NAME))
 
