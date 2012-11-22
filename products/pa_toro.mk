b61c5eea1099cf0474a9d4cee67566d82222a6f2
wjb <jaybob413@gmail.com>
2012-11-21 22:21:15 -0500

    Update pa_toro.mk product file for 4.2
    Change-Id: I8cc13f48d0bc6ebd8c116ceb3ea5b4d98f1f377e
    
    Conflicts:
    
    	products/pa_toro.mk

--- products/pa_toro.mk
+++ products/pa_toro.mk
@@ -1,3 +1,17 @@
+# Copyright (C) 2012 ParanoidAndroid Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Check for target product
 ifeq (pa_toro,$(TARGET_PRODUCT))
 
@@ -7,10 +21,25 @@ PARANOID_BOOTANIMATION_NAME := XHDPI
 # OVERLAY_TARGET adds overlay asset source
 OVERLAY_TARGET := pa_xhdpi
 
-# include ParanoidAndroid common configuration
+# Build paprefs from sources
+PREFS_FROM_SOURCE := true
+
+# Include ParanoidAndroid common configuration
 include vendor/pa/config/pa_common.mk
 
+# Include missing proprietaries
+#PRODUCT_COPY_FILES += \
+#  vendor/pa/proprietary/toro/bcm4330.hcd:system/vendor/firmware/bcm4330.hcd
+
+# Inherit AOSP device configuration
+$(call inherit-product, device/samsung/toro/full_toro.mk)
+
+# Override AOSP build properties
 PRODUCT_NAME := pa_toro
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Galaxy Nexus
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_FINGERPRINT="google/yakju/maguro:4.2/JOP40C/527662:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.2 JOP40C 527662 release-keys"
 
 GET_VENDOR_PROPS := $(shell vendor/pa/tools/getvendorprops.py $(PRODUCT_NAME))
 
