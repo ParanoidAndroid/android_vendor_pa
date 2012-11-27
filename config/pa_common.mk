e4dd2c2ef1a5bde8080be44231c26ef4656145ca
Jesús David <jedga_95@outlook.com>
2012-11-27 08:37:22 -0500

    Cleanup vendor files
    Temporally replace XHDPI bootanimation
    
    Change-Id: I21e304519fa8eedaa725bf409bd89759318ebbe9

--- config/pa_common.mk
+++ config/pa_common.mk
@@ -36,7 +36,7 @@ ifneq ($(PARANOID_BOOTANIMATION_NAME),)
         vendor/pa/prebuilt/common/bootanimation/$(PARANOID_BOOTANIMATION_NAME).zip:system/media/bootanimation.zip
 else
     PRODUCT_COPY_FILES += \
-        vendor/pa/prebuilt/common/bootanimation/HDPI.zip:system/media/bootanimation.zip
+        vendor/pa/prebuilt/common/bootanimation/XHDPI.zip:system/media/bootanimation.zip
 endif
 
 # ParanoidAndroid common packages
