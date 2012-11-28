6c49f44d75e2ad72a9633d8aa742f7901596c60c
wjb <jaybob413@gmail.com>
2012-11-28 18:32:17 -0500

    Add mako (Nexus 4)
    Change-Id: If43d8346414172c523a526813a13d6884325cf10

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -4,6 +4,9 @@ endif
 ifeq (pa_maguro,$(TARGET_PRODUCT))
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_maguro.mk
 endif
+ifeq (pa_mako,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_mako.mk
+endif
 ifeq (pa_manta,$(TARGET_PRODUCT))
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_manta.mk
 endif
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,6 @@
 add_lunch_combo pa_grouper-userdebug
 add_lunch_combo pa_maguro-userdebug
+add_lunch_combo pa_mako-userdebug
 add_lunch_combo pa_manta-userdebug
 add_lunch_combo pa_toro-userdebug
 add_lunch_combo pa_toroplus-userdebug
