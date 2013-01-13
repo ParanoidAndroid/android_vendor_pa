9d36749c1eeed0d2809f8081acbcaac3b38fa311
wjb <jaybob413@gmail.com>
2013-01-13 09:48:52 -0500

    toroplus: update local packages
    Change-Id: I417781df37633ce40160e9bd56bf33f5dd078e0e

--- products/pa_toroplus.mk
+++ products/pa_toroplus.mk
@@ -30,6 +30,12 @@ include vendor/pa/config/pa_common.mk
 # Inherit AOSP device configuration
 $(call inherit-product, device/samsung/toroplus/full_toroplus.mk)
 
+# Product Package Extras - Repos can be added manually or via addprojects.py
+-include vendor/pa/packages/toroplus.mk
+
+# CM Package Extras
+-include vendor/pa/packages/cm.mk
+
 # Override AOSP build properties
 PRODUCT_NAME := pa_toroplus
 PRODUCT_BRAND := Google
@@ -37,7 +43,11 @@ PRODUCT_MODEL := Galaxy Nexus
 PRODUCT_MANUFACTURER := Samsung
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_FINGERPRINT="google/yakju/maguro:4.2/JOP40C/527662:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.2 JOP40C 527662 release-keys"
 
+# Update local_manifest.xml
 GET_VENDOR_PROPS := $(shell vendor/pa/tools/getvendorprops.py $(PRODUCT_NAME))
+GET_PROJECT_RMS := $(shell vendor/pa/tools/removeprojects.py $(PRODUCT_NAME))
+GET_PROJECT_ADDS := $(shell vendor/pa/tools/addprojects.py $(PRODUCT_NAME))
+GET_CM_PROJECT_ADDS := $(shell vendor/pa/tools/addprojects.py cm.adds)
 
 endif
 
