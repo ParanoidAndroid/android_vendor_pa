095985d91722f6a86b04eb01f740179b7667ec67
aaronpoweruser <atlrudeboi1@gmail.com>
2013-02-23 09:02:42 -0800

    Update to supersu 1.10 multiuser edition
    Change-Id: I80142679ed62a5d0ab1d4a970b0766672138815f

--- config/pa_common.mk
+++ config/pa_common.mk
@@ -8,7 +8,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     vendor/pa/prebuilt/common/apk/GooManager.apk:system/app/GooManager.apk \
     vendor/pa/prebuilt/common/apk/SuperSU.apk:system/app/SuperSU.apk \
-    vendor/pa/prebuilt/common/xbin/su:system/xbin/su
+    vendor/pa/prebuilt/common/xbin/su:system/xbin/su \
+    vendor/pa/prebuilt/common/bin/.ext/.su:system/bib/.ext/.su
 
 # init.d support
 PRODUCT_COPY_FILES += \
