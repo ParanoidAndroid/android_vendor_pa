c129ca3119862e9df8aa01ee038e4ed31373cc93
drcmda <drcmda@gmail.com>
2013-07-30 11:25:42 +0100

    cleanup vendor (maka)
    Change-Id: I83493581e6b9178d186f4780c2bc7652d8adb778

--- config/themes_common.mk
+++
@@ -1,8 +0,0 @@
-# T-Mobile theme engine
-PRODUCT_PACKAGES += \
-       ThemeManager \
-       ThemeChooser \
-       com.tmobile.themes 
-
-PRODUCT_COPY_FILES += \
-       vendor/pa/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
