007dee2bc3b10068941716308696929dab58b2dd
Carlo Savignano <stevewatersy@gmail.com>
2013-08-04 20:31:44 +0200

    pa_tilapia.mk: Update fingerprint

--- products/pa_tilapia.mk
+++ products/pa_tilapia.mk
@@ -35,8 +35,8 @@ PRODUCT_NAME := pa_tilapia
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := Asus
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT="google/nakasig/tilapia:4.2/JOP40C/527662:user/release-keys"
-PRIVATE_BUILD_DESC="nakasi-user 4.2 JOP40C 527662 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT="google/nakasig/tilapia:4.3/JSS15J/737497:user/release-keys"
+PRIVATE_BUILD_DESC="nakasi-user 4.3 JSS15J 737497 release-keys"
 
 GET_VENDOR_PROPS := $(shell vendor/pa/tools/getvendorprops.py $(PRODUCT_NAME))
 
