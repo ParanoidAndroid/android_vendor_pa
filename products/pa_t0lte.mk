625c769b9ccdfa584ee3112821e935effe61e6ba
Joao <simoesj90@gmail.com>
2013-08-05 14:26:46 -0500

    Add t0lte

---
+++ products/pa_t0lte.mk
@@ -0,0 +1,49 @@
+# Copyright (C) 2012 ParanoidAndroid Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check for target product
+ifeq (pa_t0lte,$(TARGET_PRODUCT))
+
+# Define PA bootanimation size
+PARANOID_BOOTANIMATION_NAME := XHDPI
+
+# OVERLAY_TARGET adds overlay asset source
+OVERLAY_TARGET := pa_xhdpi
+
+# Build paprefs from sources
+PREFS_FROM_SOURCE ?= false
+
+# Include ParanoidAndroid common configuration
+include vendor/pa/config/pa_common.mk
+
+# Include missing proprietaries
+#PRODUCT_COPY_FILES += \
+
+# Inherit AOSP device configuration
+$(call inherit-product, device/samsung/t0lte/full_t0lte.mk)
+
+# CM Package Extras
+-include vendor/pa/packages/cm.mk
+
+# Override AOSP build properties
+PRODUCT_NAME := pa_t0lte
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := GT-N7105
+PRODUCT_MANUFACTURER := samsung
+
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=t0lte TARGET_DEVICE=t0lte BUILD_FINGERPRINT="samsung/t0ltexx/t0lte:4.1.2/JZO54K/N7105XXDLL4:user/release-keys" PRIVATE_BUILD_DESC="t0ltexx-user 4.1.2 JZO54K N7105XXDLL4 release-keys"
+
+endif
+
