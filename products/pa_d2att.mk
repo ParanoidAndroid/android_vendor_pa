cf4c677b71d2b856753a1aaee0e8266df88a88b8
ayysir <dresadd09691@gmail.com>
2013-08-07 02:06:25 -0400

    GS3 (D2) U.S. Variants support
    OS = jb43
    
    d2vzw = GS3 Verizon
    d2spr = GS3 Sprint
    d2tmo = GS3 T-Mobile
    d2att = GS3 AT&T
    d2mtr = GS3 Metropcs
    d2usc = GS3 US Cellular
    
    Signed-off-by: ayysir <dresadd09691@gmail.com>

---
+++ products/pa_d2att.mk
@@ -0,0 +1,57 @@
+# Copyright (C) 2012 ParanoidAndroid Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check for target product
+ifeq (pa_d2att,$(TARGET_PRODUCT))
+
+# Define PA bootanimation size
+PARANOID_BOOTANIMATION_NAME := XHDPI
+
+# OVERLAY_TARGET adds overlay asset source
+OVERLAY_TARGET := pa_xdpi
+
+# Build paprefs from sources
+PREFS_FROM_SOURCE ?= false
+
+# Include ParanoidAndroid common configuration
+include vendor/pa/config/pa_common.mk
+# inherit from common d2
+-include device/samsung/d2-common/BoardConfigCommon.mk
+# inherit from the proprietary version
+-include vendor/samsung/d2att/BoardConfigVendor.mk
+
+# Inherit from d2att device
+$(call inherit-product, device/samsung/d2att/full_d2att.mk)
+# CM Package Extras
+-include vendor/pa/packages/cm.mk
+
+# Override AOSP build properties
+PRODUCT_NAME := pa_d2att
+PRODUCT_DEVICE := d2att
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := SGH-I747
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=d2uc \
+    TARGET_DEVICE=d2att \
+    BUILD_FINGERPRINT="samsung/d2uc/d2att:4.1.1/JRO03L/I747UCDLK3:user/release-keys" \
+    PRIVATE_BUILD_DESC="d2uc-user 4.1.1 JRO03L I747UCDLK3 release-keys"
+
+# Update local_manifest.xml
+GET_VENDOR_PROPS := $(shell vendor/pa/tools/getvendorprops.py $(PRODUCT_NAME))
+GET_PROJECT_RMS := $(shell vendor/pa/tools/removeprojects.py $(PRODUCT_NAME))
+GET_PROJECT_ADDS := $(shell vendor/pa/tools/addprojects.py $(PRODUCT_NAME))
+GET_CM_PROJECT_ADDS := $(shell vendor/pa/tools/addprojects.py cm.adds)
+
+endif
---
+++ products/pa_d2tmo.mk
@@ -0,0 +1,58 @@
+# Copyright (C) 2012 ParanoidAndroid Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check for target product
+ifeq (pa_d2tmo,$(TARGET_PRODUCT))
+
+# Define PA bootanimation size
+PARANOID_BOOTANIMATION_NAME := XHDPI
+
+# OVERLAY_TARGET adds overlay asset source
+OVERLAY_TARGET := pa_xdpi
+
+# Build paprefs from sources
+PREFS_FROM_SOURCE ?= false
+
+# Include ParanoidAndroid common configuration
+include vendor/pa/config/pa_common.mk
+# inherit from common d2
+-include device/samsung/d2-common/BoardConfigCommon.mk
+# inherit from the proprietary version
+-include vendor/samsung/d2tmo/BoardConfigVendor.mk
+
+# Inherit from d2tmo device
+$(call inherit-product, device/samsung/d2tmo/full_d2tmo.mk)
+
+# CM Package Extras
+-include vendor/pa/packages/cm.mk
+
+
+# Override AOSP build properties
+PRODUCT_NAME := pa_d2tmo
+PRODUCT_DEVICE := d2tmo
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := SGH-T999
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=d2uc \
+    TARGET_DEVICE=d2tmo \
+    BUILD_FINGERPRINT="samsung/d2uc/d2att:4.1.2/JZO54K/T999UVDMD5:user/release-keys" \
+    PRIVATE_BUILD_DESC="d2uc-user 4.1.2 JZO54K T999UVDMD5 release-keys"
+
+# Update local_manifest.xml
+GET_VENDOR_PROPS := $(shell vendor/pa/tools/getvendorprops.py $(PRODUCT_NAME))
+GET_PROJECT_RMS := $(shell vendor/pa/tools/removeprojects.py $(PRODUCT_NAME))
+GET_PROJECT_ADDS := $(shell vendor/pa/tools/addprojects.py $(PRODUCT_NAME))
+GET_CM_PROJECT_ADDS := $(shell vendor/pa/tools/addprojects.py cm.adds)
+endif
