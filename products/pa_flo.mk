fd628356091f82f66168bd40936f82ecfe851117
slickrick <rsalinas1@mail.usf.edu>
2013-08-07 22:36:36 -0400

    flo: use xhdpi assets

--- products/pa_flo.mk
+++ products/pa_flo.mk
@@ -19,7 +19,7 @@ ifeq (pa_flo,$(TARGET_PRODUCT))
 PARANOID_BOOTANIMATION_NAME := XHDPI
 
 # OVERLAY_TARGET adds overlay asset source
-OVERLAY_TARGET := pa_xxhdpi
+OVERLAY_TARGET := pa_xhdpi
 
 # Build paprefs from sources
 PREFS_FROM_SOURCE ?= true
