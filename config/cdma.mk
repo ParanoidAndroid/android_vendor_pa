295b776ae901b8b9dd7508aa3d3bfd3504416c60
Eugene San (eugenesan) <eugenesan@gmail.com>
2013-08-15 11:20:57 +0300

    Unify APN installtion, combined with removal of co-responding change in build
    Change-Id: I8906019d0285082e312653b18628e36801856e13

--- config/cdma.mk
+++
@@ -1,3 +0,0 @@
-# CDMA APN list
-PRODUCT_COPY_FILES += \
-    vendor/pa/prebuilt/common/etc/apns-conf-cdma.xml:system/etc/apns-conf.xml
--- config/pa_common.mk
+++ config/pa_common.mk
@@ -87,6 +87,14 @@ PRODUCT_COPY_FILES += \
     vendor/pa/prebuilt/$(PA_CONF_SOURCE).conf:system/etc/paranoid/properties.conf \
     vendor/pa/prebuilt/$(PA_CONF_SOURCE).conf:system/etc/paranoid/backup.conf
 
+ifneq ($(PRODUCT_IS_CDMA),)
+PRODUCT_COPY_FILES += \
+    vendor/pa/prebuilt/common/etc/apns-conf-cdma.xml:system/etc/apns-conf.xml
+else
+PRODUCT_COPY_FILES += \
+    vendor/pa/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+endif
+
 PA_VERSION_MAJOR = 3
 PA_VERSION_MINOR = 9
 PA_VERSION_MAINTENANCE = 6
