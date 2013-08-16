315c2d31aa004d606e9f291ee0c1f6f7a1b08764
Eugene San (eugenesan) <eugenesan@gmail.com>
2013-08-16 15:22:21 +0300

    Make products list dynamic
    Change-Id: I37ab2831a6a167d9ba651c2b9e27995dcaa4c9ce

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,11 +1,3 @@
-add_lunch_combo pa_grouper-userdebug
-add_lunch_combo pa_find5-userdebug
-add_lunch_combo pa_flo-userdebug
-add_lunch_combo pa_tilapia-userdebug
-add_lunch_combo pa_maguro-userdebug
-add_lunch_combo pa_mako-userdebug
-add_lunch_combo pa_manta-userdebug
-add_lunch_combo pa_toro-userdebug
-add_lunch_combo pa_toroplus-userdebug
-add_lunch_combo pa_crespo-userdebug
-add_lunch_combo pa_find5-userdebug
+for combo in $(find vendor/pa -name "pa_*.mk" -exec basename '{}' \; | cut -f1 -d.); do
+	add_lunch_combo ${combo}-userdebug
+done
