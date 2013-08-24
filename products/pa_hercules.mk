86d4a9b082282eb3ba4de148327769d53986fcf5
dbanto <bryan2894@gmail.com>
2013-08-24 00:31:09 -0400

    Add hercules support
    Change-Id: Ifade52c2f658d9dc31aeb12f1cb4f63075601352

---
+++ products/pa_hercules.mk
@@ -0,0 +1,49 @@
+# Copyright (C) 2012 ParanoidAndroid Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check for target product
+ifeq (pa_hercules,$(TARGET_PRODUCT))
+
+# Define PA bootanimation size
+PARANOID_BOOTANIMATION_NAME := XHDPI
+
+# OVERLAY_TARGET adds overlay asset source
+OVERLAY_TARGET := pa_hdpi
+
+# Build paprefs from sources
+PREFS_FROM_SOURCE ?= false
+
+# Include ParanoidAndroid common configuration
+include vendor/pa/config/pa_common.mk
+
+# Inherit AOSP device configuration
+$(call inherit-product, device/samsung/hercules/full_hercules.mk)
+
+# Product Package Extras - Repos can be added manually or via addprojects.py
+-include vendor/pa/packages/hercules.mk
+
+# Include CM extras
+EXTRA_CM_PACKAGES ?= true
+
+# Override AOSP build properties
+PRODUCT_NAME := pa_hercules
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SAMSUNG-SGH-T989
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-T989 TARGET_DEVICE=SGH-T989 BUILD_FINGERPRINT="samsung/SGH-T989/SGH-T989:4.1.2/JZO54K/T989UVMC6:user/release-keys" PRIVATE_BUILD_DESC="SGH-T989-user 4.1.2 IMM76D UVMC6 release-keys"
+
+# Include ParanoidAndroid repos configuration
+include vendor/pa/config/pa_addons.mk
+
+endif
