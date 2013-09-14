e2c268a33ffb9fce9a1c1ca576ee502cd2770c34
Evan Anderson <evan1124@gmail.com>
2013-09-14 14:14:10 -0400

    manifests: Fix up cm projects to allow code review
    Change-Id: I2b44db45657d964049e1356e10fd04a9a340953e
    Signed-off-by: Evan Anderson <evan1124@gmail.com>

--- tools/setupjava.sh
+++ tools/setupjava.sh
@@ -11,28 +11,39 @@ function getjava() {
 	wget -q --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com" "$javadl"
 	chmod +x $javaf
 	./$javaf &> /dev/null
+	mv $javav/* .
+	rm -r $javav $javaf
 }
 
 function checkjava() {
-	if [[ "javac -version" == *1.6* ]]; then
-		echo -e "Java SDK 1.6 found"
+	if [ $(javac -version 2>&1 | head -n 1 | grep '[ "]1\.[6][\. "$$]') ]; then
+		installed=1
+	else
+		installed=0	
 	fi
 }
 
 function setpath() {
-	export JAVA_HOME=pwd/$installdir/$javav/bin
-	export PATH=pwd/$installdir/$javav:$PATH
+	export JAVA_HOME=`pwd`/$installpath/bin/java
+	export PATH=`pwd`/$installpath/bin:$PATH
 	echo -e "Java paths set"
 }
 
-if [ ! -d "$installpath" ]; then
+#first check if system java is correct
+checkjava
+
+if [ $installed = 1 ]; then
+	echo -e "System Java is the correct version"
+  elif [ ! -d "$installpath" ]; then
 	echo -e "Error: Java not found, downloading..."
 	mkdir $installpath; cd $installpath
 	getjava
 	setpath
 	checkjava
-   else if [ -d "$installpath" ]; then
+  elif [ "-d "$installpath"" -a "$installed = 0" ]; then
 	setpath
 	checkjava
-   fi
+	if [ $installed = 1 ]; then
+		echo -e "Correct java version installed"
+	fi
 fi
