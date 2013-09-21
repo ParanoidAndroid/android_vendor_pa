fa85e441d53247f2b51ec3f2e5b587e34489b457
wjb <jaybob413@gmail.com>
2013-09-21 09:11:13 -0400

    [toroplus] add EXTRA_CM_PACKAGES
    Change-Id: I353830ab0614772a3b7651a785400ae427e18600

---
+++ packages/pa_toroplus.mk
@@ -0,0 +1,5 @@
+PRODUCT_PACKAGES += \
+    DSPManager \
+    libcyanogen-dsp \
+    CMFileManager \
+    Torch
--- products/pa_toroplus.mk
+++ products/pa_toroplus.mk
@@ -37,6 +37,9 @@ PRODUCT_MODEL := Galaxy Nexus
 PRODUCT_MANUFACTURER := Samsung
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_FINGERPRINT="google/yakju/maguro:4.3/JSS15J/737497:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.3 JSS15J 737497 release-keys"
 
+# include CM packages
+EXTRA_CM_PACKAGES = true
+
 # Include ParanoidAndroid repos configuration
 include vendor/pa/config/pa_addons.mk
 
